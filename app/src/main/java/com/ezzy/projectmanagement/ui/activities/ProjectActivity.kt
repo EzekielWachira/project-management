a2bb48ea440774875f047ba0ec9e6e02f097cd32
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-03-29 17:10:30 +0300

    :sparkles:initiate dependency injection in the app'

--- app/src/main/java/com/ezzy/projectmanagement/ui/activities/ProjectActivity.kt
+++ app/src/main/java/com/ezzy/projectmanagement/ui/activities/ProjectActivity.kt
@@ -1,11 +1,12 @@
-package com.ezzy.projectmanagement
+package com.ezzy.projectmanagement.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ezzy.projectmanagement.R
 
-class MainActivity : AppCompatActivity() {
+class ProjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_project)
     }
 }
