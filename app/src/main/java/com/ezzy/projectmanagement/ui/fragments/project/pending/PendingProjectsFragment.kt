6bcba6042bb091e42c00f75f8bd41f2afc0b6327
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-03-30 23:21:23 +0300

    :sparkles:add projects fragments

---
+++ app/src/main/java/com/ezzy/projectmanagement/ui/fragments/project/pending/PendingProjectsFragment.kt
@@ -0,0 +1,31 @@
+package com.ezzy.projectmanagement.ui.fragments.project.pending
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ezzy.projectmanagement.R
+import com.ezzy.projectmanagement.databinding.FragmentPendingProjectsBinding
+
+class PendingProjectsFragment : Fragment() {
+
+    private var _binding : FragmentPendingProjectsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPendingProjectsBinding.inflate(
+            inflater, container, false
+        )
+        return binding.root
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
