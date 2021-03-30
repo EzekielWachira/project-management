5ff591422c832218fab1160be7a836ae913300b6
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-03-30 18:00:07 +0300

    :sparkles:set up bottom navbar with navcontroller

--- app/src/main/java/com/ezzy/projectmanagement/ui/activities/project/ProjectActivity.kt
+++ app/src/main/java/com/ezzy/projectmanagement/ui/activities/project/ProjectActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.ezzy.projectmanagement.R
 import com.ezzy.projectmanagement.databinding.ActivityProjectBinding
 import com.ezzy.projectmanagement.ui.activities.auth.LoginActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -19,6 +24,7 @@ class ProjectActivity : AppCompatActivity() {
     lateinit var firebaseAuth: FirebaseAuth
 
     private lateinit var binding : ActivityProjectBinding
+    private lateinit var projectNavHostContainer : FragmentContainerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,7 +38,35 @@ class ProjectActivity : AppCompatActivity() {
         } else {
             makeToast("You are already logged in")
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.projectNavHostContainer) as NavHostFragment
+        val navController = this.findNavController(R.id.projectNavHostContainer)
+
+//        projectNavHostContainer = binding.projectNavHostContainer
+
+        binding.bottomNavigation.setupWithNavController(
+            navController
+        )
     }
+//
+//    private fun setUpBottomNavigation() {
+//        binding.bottomNavigation.setOnNavigationItemReselectedListener { menuItem ->
+//            when(menuItem.itemId) {
+//                R.id.projectFragment -> {
+//
+//                }
+//                R.id.activityFragment -> {
+//                    TODO("push navigation fragments")
+//                }
+//                R.id.calendarFragment -> {
+//                    TODO("push navigation fragments")
+//                }
+//                R.id.profileFragment -> {
+//                    TODO("push navigation fragments")
+//                }
+//            }
+//        }
+//    }
 
     private fun makeToast(message : String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
