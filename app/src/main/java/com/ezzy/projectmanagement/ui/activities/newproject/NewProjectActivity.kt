b63893baa5c6401d368115c3783dcfc7a96df226
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-03-31 13:56:22 +0300

    :lipstick: design new project activity

--- app/src/main/java/com/ezzy/projectmanagement/ui/activities/newproject/NewProjectActivity.kt
+++ app/src/main/java/com/ezzy/projectmanagement/ui/activities/newproject/NewProjectActivity.kt
@@ -2,6 +2,7 @@ package com.ezzy.projectmanagement.ui.activities.newproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.ezzy.projectmanagement.R
 import com.ezzy.projectmanagement.databinding.ActivityNewProjectBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,5 +16,13 @@ class NewProjectActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityNewProjectBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val projectStages = resources.getStringArray(R.array.project_stages)
+        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_item, projectStages)
+        binding.projectStageTextView.setAdapter(arrayAdapter)
     }
 }
