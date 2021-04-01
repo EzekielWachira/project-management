272d3686e4fb1d79bced334f32b748841f33856e
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-04-02 00:34:21 +0300

    :lipstick: design add members dialog

--- app/src/main/java/com/ezzy/projectmanagement/ui/dialogs/AddMembersDialog.kt
+++ app/src/main/java/com/ezzy/projectmanagement/ui/dialogs/AddMembersDialog.kt
@@ -3,18 +3,29 @@ package com.ezzy.projectmanagement.ui.dialogs
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
 import com.ezzy.projectmanagement.R
 
 class AddMembersDialog : DialogFragment() {
+
+    private lateinit var doneButton : Button
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
 
         val builder = AlertDialog.Builder(activity)
         val inflator = activity?.layoutInflater
         val view = inflator?.inflate(
-            R.layout.new_project_bottom_sheet, null
+            R.layout.layout_add_members_dialog, null
         )
+
+        view?.let {
+            doneButton = view.findViewById(R.id.buttonDone)!!
+        }
+
+        doneButton.setOnClickListener {
+            dialog?.dismiss()
+        }
 
         builder.setView(view)
 
