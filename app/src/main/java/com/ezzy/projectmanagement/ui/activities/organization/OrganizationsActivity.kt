412011a291588d2bfc4820cc54835853f8248156
Ezekiel Wachira <ezekielwachira048@gmail.com>
2021-04-08 16:54:05 +0300

    :sparkles:search organizations

--- app/src/main/java/com/ezzy/projectmanagement/ui/activities/organization/OrganizationsActivity.kt
+++ app/src/main/java/com/ezzy/projectmanagement/ui/activities/organization/OrganizationsActivity.kt
@@ -2,7 +2,11 @@ package com.ezzy.projectmanagement.ui.activities.organization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
+import androidx.activity.viewModels
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ezzy.projectmanagement.adapters.viewpager.OrganizationsAdapter
@@ -13,6 +17,7 @@ import com.ezzy.projectmanagement.util.VerticalItemDecorator
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -24,7 +29,7 @@ class OrganizationsActivity : AppCompatActivity() {
     lateinit var firestore: FirebaseFirestore
     @Inject
     lateinit var storage: FirebaseStorage
-    private lateinit var organizationViewModel: OrganizationViewModel
+    private val organizationViewModel: OrganizationViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,11 +41,36 @@ class OrganizationsActivity : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
         }
 
-        organizationViewModel = OrganizationViewModel(application, firestore, storage)
-
         setUpRecyclerView()
 
         organizationViewModel.retrieveOrganizations()
+
+        binding.searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binding.searchEditText.text.isNotEmpty()){
+                    organizationViewModel.searchOrganizations(
+                        binding.searchEditText.text.toString().toLowerCase(
+                            Locale.getDefault()
+                        )
+                    )
+                } else {
+                    organizationViewModel.retrieveOrganizations()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (binding.searchEditText.text.isNotEmpty()){
+                    organizationViewModel.searchOrganizations(
+                        s.toString().toLowerCase(Locale.getDefault())
+                    )
+                } else {
+                    organizationViewModel.retrieveOrganizations()
+                }
+            }
+
+        })
 
         organizationViewModel.organizations.observe(this, { organizationList ->
             orgAdapter.differ.submitList(organizationList)
@@ -52,6 +82,10 @@ class OrganizationsActivity : AppCompatActivity() {
             } else {
                 binding.orgProgressBar.visibility = View.VISIBLE
             }
+        })
+
+        organizationViewModel.orgsSearched.observe(this, { orgsList ->
+            orgAdapter.differ.submitList(orgsList)
         })
     }
 
